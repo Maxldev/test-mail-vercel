af02bfb9bbd9d6f8491389aa176d5671c9f70ffa
Maxldev <maximeleraydev@gmail.com>
2021-07-12 09:55:57 +0200

    test mail

---
+++ pages/api/contact.js
@@ -0,0 +1,30 @@
+export default (req, res) => {
+    require('dotenv').config()
+    
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+        user: process.env.mailsender,
+        pass: process.env.mailpw,
+      },
+      secure: true,
+    })
+    const mailData = {
+      from: process.env.mailsender,
+      to: process.env.mailreceive,
+      subject: `Message From ${req.body.name}`,
+      text: req.body.message + " | Sent from: " + req.body.email,
+      html: `<div>${req.body.message}</div><p>Sent from:
+      ${req.body.email}</p>`
+    }
+    transporter.sendMail(mailData, function (err, info) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info)
+    })
+    res.status(200)
+    res.end();
+  }
